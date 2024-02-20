7d600d574bf83b791e4197aa520ab7925f0a2121
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-02-20 15:35:27 +0300

    T4: Make Responsive Header -> Responsive is done Burger Menu Added

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -8,15 +8,17 @@ import {
   FaSearch,
   FaRegHeart,
 } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
+
 import { CiMail } from "react-icons/ci";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <div className="">
+    <div className="md:w-full">
       {/* Dark NavBar */}
-      <div className="max-w-full bg-[#252B42]">
+      <div className="max-w-full bg-[#252B42] md:hidden">
         <div className="flex flex-row flex-wrap md:flex-col max-w-[1440px] mx-auto justify-between items-center  min-h-[58px] px-[1.5em]">
           <div className="flex items-center gap-[1.5rem] ">
             <div className="inline-flex  items-center gap-[0.25rem]">
@@ -48,32 +50,32 @@ export const Header = () => {
       </div>
       {/*Navbar Light */}
       <div className="flex justify-between items-center px-[1.5em] max-w-[1440px] mx-auto md:flex-col flex-wrap">
-        <nav className="inline-flex items-center gap-[2rem] md:flex-col md:w-full">
-          <div className="py-4 ">
+        <nav className="inline-flex items-center gap-[2rem] w-full justify-between md:flex-wrap">
+          <div className="flex py-4  md:inline-flex">
             <h3 className="text-2xl font-bold">Bandage</h3>
           </div>
-          <div className="flex gap-[1rem] font-bold text-[#737373] text-sm ml-20 md:ml-0 md:flex-col">
+          <div className="flex gap-[1rem] font-bold text-[#737373] text-sm ml-20 md:ml-0 md:flex-col md:w-full md:items-center md:text-xl md:order-last md:mb-12">
             <Link to={"/"}>Home</Link>
             <Link to={"/shop"}>Shop</Link>
             <Link to={"/about"}>About</Link>
-            <Link to={"/blog"}>Blog</Link>
             <Link to={"/contact"}>Contact</Link>
             <Link to={"/product"}>Pages</Link>
           </div>
+          <div className="inline-flex justify-end items-center text-[#23A6F0] gap-6">
+            <div className="flex gap-2 items-center md:hidden">
+              <FaRegUser />
+              <span>Login</span>
+              <span>/</span>
+              <span>Register</span>
+            </div>
+            <div className="flex gap-4 flex-wrap md:w-full">
+              <FaSearch className="size-5" />
+              <MdOutlineShoppingCart className="size-5" />
+              <FaRegHeart className="size-5 md:hidden" />
+              <RxHamburgerMenu className="size-5 hidden md:inline-flex" />
+            </div>
+          </div>
         </nav>
-        <div className="inline-flex justify-end items-center text-[#23A6F0] gap-6">
-          <div className="flex gap-2 items-center sm:hidden">
-            <FaRegUser />
-            <span>Login</span>
-            <span>/</span>
-            <span>Register</span>
-          </div>
-          <div className="flex gap-4 flex-wrap">
-            <FaSearch className="size-5" />
-            <MdOutlineShoppingCart className="size-5" />
-            <FaRegHeart className="size-5" />
-          </div>
-        </div>
       </div>
     </div>
   );
