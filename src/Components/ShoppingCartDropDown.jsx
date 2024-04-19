33f7cfd1c0820c554ed7330017f741b5ba96a5f0
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-04-19 16:49:17 +0300

    T14: -ShoppingCart Css changes -Count feature is added.

--- src/Components/ShoppingCartDropDown.jsx
+++ src/Components/ShoppingCartDropDown.jsx
@@ -1,15 +1,21 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { updateCartItemCount } from "../store/actions/shoppingCartAction";
 
 export const ShoppingCartDropDown = () => {
+  const dispatch = useDispatch();
   const cartItems = useSelector((store) => store.shoppingCart.cart);
   const cartLenght = cartItems.length;
   //Genderdan bize -> k ya da e dönüyor.
 
+  // const updateItemCountHandler = (item) => {
+  //   dispatch(updateCartItemCount(item.id, existingItem.count + 1));
+  // }
+
   return (
-    <div className="mx-auto flex items-center justify-center ">
-      <div className="group relative cursor-pointer ">
+    <div className="relative mx-auto flex items-center justify-center ">
+      <div className="group  cursor-pointer ">
         <div className="flex items-center justify-between  bg-white ">
           <a className="menu-hover flex">
             <MdOutlineShoppingCart className="size-5 cursor-pointer" />
@@ -34,31 +40,56 @@ export const ShoppingCartDropDown = () => {
             </svg>
           </span>
         </div>
-        <div className="invisible absolute  flex w-[300px] flex-col bg-gray-100 py-1 px-2 text-gray-800 shadow-xl group-hover:visible">
+        <div className="invisible absolute flex w-[350px] max-h-[350px] flex-col bg-gray-100 py-1 px-2 text-gray-800 shadow-xl group-hover:visible right-0 overflow-auto">
           {cartItems.map((item, index) => (
             <div
-              className="my-2 block border-b border-gray-100 py-1  hover:text-black md:mx-2"
+              className="my-2 block border-b border-gray-300 py-1  hover:text-black md:mx-2 "
               key={index}
             >
-              <div className="flex flex-row">
+              <div className="flex flex-row justify-around">
                 <img
                   src={item.product.images[0].url}
                   alt={item.product.id}
-                  className="w-[100px] h-[70px]"
+                  className="w-[100px] h-[120px]"
                 />
-                <div className="flex flex-col">
-                  {item.product.description}
+                <div className="flex flex-col gap-2">
+                  <span className="font-semibold">{item.product.name}</span>
                   <div>
-                    {`Beden: 32`} {`Adet: ${item.count}`}
+                    <span className="font-semibold text-green-600 text-sm">{`$${
+                      item.product.price * item.count
+                    }`}</span>
                   </div>
-                  <span className="">{item.product.price}</span>
+                  <div className="flex gap-2 font-semibold">
+                    <button
+                      className="bg-pBlue px-2 rounded-md text-white"
+                      onClick={() =>
+                        dispatch(
+                          updateCartItemCount(item.product.id, item.count - 1)
+                        )
+                      }
+                    >
+                      -
+                    </button>
+                    <span>{item.count}</span>
+                    <button
+                      className="bg-pBlue px-2 rounded-md text-white"
+                      onClick={() =>
+                        dispatch(
+                          updateCartItemCount(item.product.id, item.count + 1)
+                        )
+                      }
+                    >
+                      +
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           ))}
-          <div className="flex flex-row justify-between">
-            <Link className="px-2 py-2">Sepete Git</Link>
-            <Link className="px-2 py-2">Siparişi Tamamla</Link>
+          <div className="flex flex-row justify-between mx-auto">
+            <Link className="bg-pBlue px-6 py-2 rounded-md text-white font-semibold">
+              Go to Cart
+            </Link>
           </div>
         </div>
       </div>
